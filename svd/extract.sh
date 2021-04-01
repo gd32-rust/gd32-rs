a93b73f6891d138917474051bff2c3ab72c8e6da
Andrew Walbran <qwandor@google.com>
2021-04-01 15:30:39 +0000

    Add copyright headers.

--- svd/extract.sh
+++ svd/extract.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Copyright 2021 The gd32-rs authors.
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
 # Copy GD32 SVD files and give them lower-case names.
 cp vendor/GD32F1x0.svd gd32f1x0.svd
