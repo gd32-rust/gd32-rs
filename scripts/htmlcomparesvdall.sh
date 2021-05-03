458459087ed5ef05ba642f0be2181a2bb4b5b911
Andrew Walbran <qwandor@google.com>
2021-05-03 18:15:33 +0100

    Fix compare script.

--- scripts/htmlcomparesvdall.sh
+++ scripts/htmlcomparesvdall.sh
@@ -4,15 +4,8 @@ set -euxo pipefail
 mkdir html
 
 mkdir html/gd32f
-python3 scripts/htmlcomparesvd.py svd/gd32f{130,170}.svd.patched
-sed -i 's#<table>#<p>Only a representative member of each family included; click to view entire family</p><table>#' html/index.html
-sed -i 's#gd32f130#<a href="gd32f1/index.html">GD32F130</a>#' html/index.html
-sed -i 's#gd32f170#<a href="gd32f1/index.html">GD32F170</a>#' html/index.html
+python3 scripts/htmlcomparesvd.py svd/gd32f*.svd.patched
 mv html/*.html html/gd32f
-
-mkdir html/gd32f/gd32f1
-python3 scripts/htmlcomparesvd.py svd/gd32f1*.svd.patched
-mv html/*.html html/gd32f/gd32f1
 
 cat > html/index.html <<EOF
 <!DOCTYPE html>
