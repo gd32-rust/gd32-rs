1b565f41e27c0d558806809c093e584fa1554d31
Andrew Walbran <qwandor@google.com>
2022-06-21 17:19:24 +0100

    Generate peripheral comparisons per family.

--- scripts/htmlcomparesvdall.sh
+++ scripts/htmlcomparesvdall.sh
@@ -4,6 +4,13 @@ set -euxo pipefail
 mkdir -p html/gd32
 python3 scripts/htmlcomparesvd.py html/gd32 svd/gd32*.svd.patched
 
+mkdir -p html/gd32e2
+python3 scripts/htmlcomparesvd.py html/gd32e2 svd/gd32e2*.svd.patched
+mkdir -p html/gd32f1
+python3 scripts/htmlcomparesvd.py html/gd32f1 svd/gd32f1*.svd.patched
+mkdir -p html/gd32f3
+python3 scripts/htmlcomparesvd.py html/gd32f3 svd/gd32f3*.svd.patched
+
 cat > html/comparisons.html <<EOF
 <!DOCTYPE html>
 <head>
@@ -30,7 +37,10 @@ cat > html/comparisons.html <<EOF
 
 <h1>Device families</h1>
 <ul>
-  <li><a href="gd32/index.html">GD32</a></li>
+  <li><a href="gd32/index.html">GD32 overview</a></li>
+  <li><a href="gd32e2/index.html">GD32E2</a></li>
+  <li><a href="gd32f1/index.html">GD32F1</a></li>
+  <li><a href="gd32f3/index.html">GD32F3</a></li>
 </ul>
 </body>
 </html>
